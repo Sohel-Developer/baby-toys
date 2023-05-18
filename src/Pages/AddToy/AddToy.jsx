5419cb55116c3f41ca854fe566ec7d63e9194178
Sohel-Developer <sohel.webdevoloper.com>
2023-05-18 19:16:14 +0600

    Add Toy Page added

--- src/Pages/AddToy/AddToy.jsx
+++ src/Pages/AddToy/AddToy.jsx
@@ -4,10 +4,86 @@ import useDocumentTitle from '../../useDocumentTitle/useDocumentTitle';
 const AddToy = () => {
     /* Upadte Title */
     useDocumentTitle('Baby-Toy | AddToy')
+
+    const handleAddToy = (e) => {
+
+    }
+
     return (
-        <div>
-            <h2 className="text-3xl">Add Ay Toy</h2>
-        </div>
+        <div className=" mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300">
+            <div className='text-center space-y-2'>
+                <h1 className="text-4xl font-medium">Add Toy</h1>
+            </div>
+            <form onSubmit={handleAddToy} className="my-10">
+                <div className="flex flex-col space-y-5">
+                    <div className='flex gap-5'>
+                        <div className='w-1/2'>
+                            <label htmlFor="email w-full">
+                                <p className="font-medium text-slate-700 pb-2">Seller Name</p>
+                                <input id="email" required name="email" type="email" className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Enter email address" />
+                            </label>
+                        </div>
+                        <div className='w-1/2'>
+                            <label htmlFor="password">
+                                <p className="font-medium text-slate-700 pb-2">Seller Email</p>
+                                <input id="password" name="password" required type="password" className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Enter your password" />
+                            </label>
+                        </div>
+                    </div>
+                    <div className='flex gap-5 '>
+                        <div className='w-1/2'>
+                            <label htmlFor="email w-full">
+                                <p className="font-medium text-slate-700 pb-2">Toy Name</p>
+                                <input id="email" required name="email" type="email" className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Enter email address" />
+                            </label>
+                        </div>
+                        <div className='w-1/2'>
+                            <label htmlFor="password">
+                                <p className="font-medium text-slate-700 pb-2">Price</p>
+                                <input id="password" name="password" required type="password" className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Enter your password" />
+                            </label>
+                        </div>
+                    </div>
+                    <div className='flex gap-5'>
+                        <div className='w-1/2'>
+                            <label htmlFor="email w-full">
+                                <p className="font-medium text-slate-700 pb-2">Quantity</p>
+                                <input id="email" required name="email" type="email" className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Enter email address" />
+                            </label>
+                        </div>
+                        <div className='w-1/2'>
+                            <label htmlFor="password">
+                                <p className="font-medium text-slate-700 pb-2">Picture</p>
+                                <input id="password" name="password" required type="password" className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Enter your password" />
+                            </label>
+                        </div>
+                    </div>
+                    <div className='flex gap-5 '>
+                        <div className='w-1/2'>
+                            <label htmlFor="email w-full">
+                                <p className="font-medium text-slate-700 pb-2">Rating</p>
+                                <input id="email" required name="email" type="email" className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Enter email address" />
+                            </label>
+                        </div>
+                        <div className='w-1/2'>
+                            <p className="font-medium text-slate-700 pb-2">Category</p>
+
+                            <select id='category' className="select select-primary w-full max-w-xs">
+                                <option value="bus">bus</option>
+                                <option value="truck">truck</option>
+                                <option value="ambulance">ambulance</option>
+                            </select>
+
+                        </div>
+                    </div>
+                    <button type='submit' className="w-full py-3 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center">
+                        <span>Add Toy</span>
+                    </button>
+
+
+                </div >
+            </form >
+        </div >
     );
 };
 
--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -3,10 +3,125 @@ import useDocumentTitle from '../../useDocumentTitle/useDocumentTitle';
 
 const AllToys = () => {
     useDocumentTitle('Baby-Toy | AllToys')
+
+
+
+
     return (
         <div>
-            <h2 className="text-4xl">All Toyss</h2>
-        </div>
+            <div className="flex items-center justify-center min-h-screen">
+                <div className=" w-2/3 mx-auto">
+                    <div className="overflow-auto  lg:overflow-visible ">
+                        <table className="table w-full text-gray-400 border-separate space-y-6 text-sm">
+                            <thead className="bg-gray-800 text-gray-500">
+                                <tr>
+                                    <th className="p-3">Toy Cars</th>
+                                    <th className="p-3 text-left">Category</th>
+                                    <th className="p-3 text-left">Price</th>
+                                    <th className="p-3 text-left">Quantity</th>
+                                    <th className="p-3 text-left">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody >
+                                <tr className="bg-gray-800">
+                                    <td className="p-3">
+                                        <div className="flex align-items-center">
+                                            <img className="rounded-full h-12 w-12  object-cover" src="https://images.unsplash.com/photo-1613588718956-c2e80305bf61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80" alt="unsplash image" />
+                                            <div className="ml-3">
+                                                <div className="">Appple</div>
+                                                <div className="text-gray-500">mail@rgmail.com</div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td className="p-3">
+                                        Technology
+                                    </td>
+                                    <td className="p-3 font-bold">
+                                        200.00$
+                                    </td>
+                                    <td className="p-3">
+                                        <span className="bg-green-400 text-gray-50 rounded-md px-2">available</span>
+                                    </td>
+                                    <td className="p-3 ">
+                                        <a href="#" className="text-gray-400 hover:text-gray-900 mr-2">
+                                            <i className="material-icons-outlined text-base">visibility</i>
+                                        </a>
+                                        <a href="#" className="text-gray-400 hover:text-gray-100  mx-2">
+                                            <i className="material-icons-outlined text-base">edit</i>
+                                        </a>
+                                        <a href="#" className="text-gray-400 hover:text-gray-100  ml-2">
+                                            <i className="material-icons-round text-base">delete_outline</i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr className="bg-gray-800">
+                                    <td className="p-3">
+                                        <div className="flex align-items-center">
+                                            <img className="rounded-full h-12 w-12   object-cover" src="https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="unsplash image" />
+                                            <div className="ml-3">
+                                                <div className="">Realme</div>
+                                                <div className="text-gray-500">mail@rgmail.com</div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td className="p-3">
+                                        Technology
+                                    </td>
+                                    <td className="p-3 font-bold">
+                                        200.00$
+                                    </td>
+                                    <td className="p-3">
+                                        <span className="bg-red-400 text-gray-50 rounded-md px-2">no stock</span>
+                                    </td>
+                                    <td className="p-3">
+                                        <a href="#" className="text-gray-400 hover:text-gray-100  mr-2">
+                                            <i className="material-icons-outlined text-base">visibility</i>
+                                        </a>
+                                        <a href="#" className="text-gray-400 hover:text-gray-100 mx-2">
+                                            <i className="material-icons-outlined text-base">edit</i>
+                                        </a>
+                                        <a href="#" className="text-gray-400 hover:text-gray-100 ml-2">
+                                            <i className="material-icons-round text-base">delete_outline</i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr className="bg-gray-800">
+                                    <td className="p-3">
+                                        <div className="flex align-items-center">
+                                            <img className="rounded-full h-12 w-12   object-cover" src="https://images.unsplash.com/photo-1600856209923-34372e319a5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2135&q=80" alt="unsplash image" />
+                                            <div className="ml-3">
+                                                <div className="">Samsung</div>
+                                                <div className="text-gray-500">mail@rgmail.com</div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td className="p-3">
+                                        Technology
+                                    </td>
+                                    <td className="p-3 font-bold">
+                                        200.00$
+                                    </td>
+                                    <td className="p-3">
+                                        <span className="bg-yellow-400 text-gray-50  rounded-md px-2">start sale</span>
+                                    </td>
+                                    <td className="p-3">
+                                        <a href="#" className="text-gray-400 hover:text-gray-100 mr-2">
+                                            <i className="material-icons-outlined text-base">visibility</i>
+                                        </a>
+                                        <a href="#" className="text-gray-400 hover:text-gray-100 mx-2">
+                                            <i className="material-icons-outlined text-base">edit</i>
+                                        </a>
+                                        <a href="#" className="text-gray-400 hover:text-gray-100 ml-2">
+                                            <i className="material-icons-round text-base">delete_outline</i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 };
 
