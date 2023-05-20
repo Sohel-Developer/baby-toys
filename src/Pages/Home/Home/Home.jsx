dcc192dd10374ae13e0b7c59bde690a3c39c014f
Sohel-Developer <sohel.webdevoloper.com>
2023-05-20 09:39:58 +0600

    Daily Offer Section Added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Feature from '../Feature/Feature';
+import Offer from '../Offer/Offer';
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <Feature />
+            <Offer />
         </div>
     );
 };
---
+++ src/Pages/Home/Offer/Offer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Offer = () => {
+    return (
+        <div>
+            <div className='text-center py-20 space-y-5 bg-pink-300 rounded-md'>
+                <h2 className='text-5xl font-bold'>Deal Of The Day</h2>
+                <p className='text-2xl font-bold'> <span className='text-pink-500'>UPTO 35% OFF</span> ON ALL OTHER BABY PRODUCTS</p>
+            </div>
+        </div>
+    );
+};
+
+export default Offer;
