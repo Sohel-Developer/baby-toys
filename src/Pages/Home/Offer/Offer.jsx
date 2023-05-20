5c569f01b7b7c7557c9315e8ce8f1fb0bc72f612
Sohel-Developer <sohel.webdevoloper.com>
2023-05-20 14:59:59 +0600

    Homepage Responsive

--- src/Pages/Home/Offer/Offer.jsx
+++ src/Pages/Home/Offer/Offer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Offer = () => {
     return (
-        <div>
+        <div className='p-5'>
             <div className='text-center py-20 space-y-5 bg-sky-100 rounded-md'>
                 <h2 className='text-5xl font-bold'>Deal Of The Day</h2>
                 <p className='text-2xl font-bold'> <span className='text-pink-500'>UPTO 35% OFF</span> ON ALL OTHER BABY PRODUCTS</p>
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="footer-1  py-8 sm:py-12">
             <div className="container mx-auto px-4">
-                <div className="sm:flex sm:flex-wrap sm:-mx-4 md:py-4">
+                <div className="sm:flex sm:flex-wrap sm:-mx-4 md:py-4 text-center md:text-left">
                     <div className="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6">
                         <h5 className="text-xl font-bold mb-6">Other Pages</h5>
                         <ul className="list-none footer-links">
